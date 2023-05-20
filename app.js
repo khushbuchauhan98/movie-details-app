5b754a0703a3262350124f980a4aacab058e297d
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:38:23 +0530

    3

--- app.js
+++ app.js
@@ -1,54 +1,49 @@
-
 let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
 let loadingstatus = false;
 function loader(){
-    let getintro = document.getElementById('intro')
-  getintro.style.display = 'none'
-  let getloader = document.getElementById('loader')
 
-  if (loadingstatus == true){
-    getloader.classList.add('loader')
-  }
-  else{
-    getloader.classList.remove('loader')
-  }
+    let getintro = document.getElementById('intro');
+    getintro.style.display = 'none';
+    let getloader = document.getElementById('loader');
+ if(loadingstatus==true)
+{
+ getloader.classList.add('loader')
 }
-
+else{
+    getloader.classList.remove('loader')
+}
+  }
+ 
 function gen(){
     loadingstatus = true;
-    loader();
-    let getmoviename = document.getElementById('getmoviename');
+    loader()
+
+    let getmoviename = document.getElementById('getmovienames');
     let apiwithmovie = api + getmoviename.value;
     console.log(apiwithmovie);
     fetch(apiwithmovie).then((response) => {
-        return response.json();
-    }).then((data) => {
-        loadingstatus = false;
-        loader();
-         let h1 = document.getElementById('h1');
-let p = document.getElementById('p');
-let img = document.getElementById('img');
-        h1.innerText = data.Title;
-        p.innerText = data.Plot;
-        img.src = data.Poster;
-        // console.log(data);
-    });
+    fetch(apiwithmovie)
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+             console.log(data);
+        })
+        .then((data) => {
+            loadingstatus = false;
+            loader()
+             let h1 = document.getElementById('h1')
+             h1.innerText = data.Title
+    let p = document.getElementById('p')
+    p.innerText = data.Plot
+    let img = document.getElementById('img')
+            img.src = data.Poster  
+        })
+             let h1 = document.getElementById('h1');
+    let p = document.getElementById('p');
+    let img = document.getElementById('img');
+            h1.innerText = data.Title;
+            p.innerText = data.Plot;
+            img.src = data.Poster;
+            console.log(data);
+        });
 }
-
-// let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t=dangal'
-
-// fetch(api)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-   
-//     console.log(data);
-//     console.log(data.Plot);
-//     let h1 = document.getElementById('h1');
-//     let p = document.getElementById('p');
-//     let img = document.getElementById('img');
-//     h1.innerText = data.Title;
-//     p.innerText = data.Plot;
-//     img.src = data.Poster;
-//   });
