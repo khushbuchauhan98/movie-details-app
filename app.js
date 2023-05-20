03a93f52dc4355bfb0ab258fbd5c87b7ac39293b
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:13:02 +0530

    fa

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-
-let api = ' https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
-let loadingstatus = false;
- loader=()=>{
-
-    let getintro = document.getElementById('intro');
+let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t='
+let loadingstatus = false
+loader = () => {
+    let getloader = document.getElementById('loader')
+    let getintro = document.getElementById('intro')
     getintro.style.display = 'none';
-    let getloader = document.getElementById('loader');
-  
-    if (loadingstatus==true) {
+    if (loadingstatus == true) {
         getloader.classList.add('loader')
     }
     else {
         getloader.classList.remove('loader')
     }
-  }
- gen=()=>{
-    loadingstatus = true;
-    loader()
+
+}
+gen = () => {
+    loadingstatus = true
+    loader();
    
-    let getmoviename = document.getElementById('getmovienames');
+    let getmoviename = document.getElementById('getmoviename');
     let apiwithmovie = api + getmoviename.value;
-    console.log(apiwithmovie);
+    // console.log(apiwithmovie);
     fetch(apiwithmovie)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             loadingstatus = false;
-            loader()
+            loader();
              let h1 = document.getElementById('h1');
     let p = document.getElementById('p');
     let img = document.getElementById('img');
             h1.innerText = data.Title;
             p.innerText = data.Plot;
             img.src = data.Poster;
-            console.log(data);
+            // console.log(data);
         });
 }
 // let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t=dangal'
