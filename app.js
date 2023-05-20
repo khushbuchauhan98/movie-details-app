fa7d210f14e3460840c1fecc91f83d6f7c430c77
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:44:19 +0530

    3

--- app.js
+++ app.js
@@ -21,29 +21,18 @@ function gen(){
     let getmoviename = document.getElementById('getmovienames');
     let apiwithmovie = api + getmoviename.value;
     console.log(apiwithmovie);
-    fetch(apiwithmovie).then((response) => {
-    fetch(apiwithmovie)
-        .then((response) => {
-            return response.json();
-        }).then((data) => {
-             console.log(data);
-        })
-        .then((data) => {
-            loadingstatus = false;
-            loader()
-             let h1 = document.getElementById('h1')
-             h1.innerText = data.Title
-    let p = document.getElementById('p')
-    p.innerText = data.Plot
-    let img = document.getElementById('img')
-            img.src = data.Poster  
-        })
-             let h1 = document.getElementById('h1');
-    let p = document.getElementById('p');
-    let img = document.getElementById('img');
-            h1.innerText = data.Title;
-            p.innerText = data.Plot;
-            img.src = data.Poster;
-            console.log(data);
-        });
+    fetch(apiwithmovie).then((response)=>{
+        return response.json()
+      }).then((data)=>{
+        console.log(data)
+        loadingstatus = false;
+    loader()
+    let img=document.getElementById('img');
+    let h1= document.getElementById('h1');
+    let p= document.getElementById('p')
+    h1.innerText=data.Title;
+    p.innerText=data.Plot;
+    console.log(data);
+    img.src=data.Poster;
+      })
 }
