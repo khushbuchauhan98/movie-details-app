09215383b911d787b94da0c1a7e9441d1df5ccbf
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:07:28 +0530

    f5

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
 let loadingstatus = false;
-function loader(){
+ loader=()=>{
 
     let getintro = document.getElementById('intro');
     getintro.style.display = 'none';
@@ -14,46 +14,7 @@ function loader(){
         getloader.classList.remove('loader')
     }
   }
-  function fetchMovieDetails(){
- 
-
-    loadingStatus = true;
-    checkLoaderStatus()
-    let movieName = document.getElementById('movieName');
-    let apiQuery = API_Movie + movieName.value
-    console.log(apiQuery)
-  
-    fetch(apiQuery).then((response)=>{
-      return response.json()
-    }).then((data)=>{
-      console.log(data)
-      loadingStatus = false;
-      checkLoaderStatus()
-      let img = document.getElementById('img')
-        img.src = data.Poster;
-        let title = document.getElementById('title')
-        title.innerText = data.Title
-        let desc = document.getElementById('desc')
-        desc.innerText = data.Plot
-        let actors = document.getElementById('actors')
-        actors.innerText = 'Actors: ' + data.Actors
-        let awards = document.getElementById('awards')
-        awards.innerText = 'Awards: ' + data.Awards
-        let boxOffice = document.getElementById('boxOffice')
-        boxOffice.innerText = 'Box Office: ' + data.BoxOffice
-        let director = document.getElementById('director')
-        director.innerText = 'Director: ' + data.Director
-        let country = document.getElementById('country')
-        country.innerText = 'Country: ' + data.Country
-        let genre = document.getElementById('genre')
-        genre.innerText = 'Genre: ' + data.Genre
-        let language = document.getElementById('language')
-        language.innerText = 'Language: ' + data.Language
-        
-    
-    })
-  }
-function gen(){
+ gen=()=>{
     loadingstatus = true;
     loader()
    
