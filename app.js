5ae9a3374cfc05fefeb8ab2992540a8bc3a7eaaa
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:06:00 +0530

    f4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
 let loadingstatus = false;
 function loader(){
+
     let getintro = document.getElementById('intro');
     getintro.style.display = 'none';
     let getloader = document.getElementById('loader');
@@ -13,26 +14,67 @@ function loader(){
         getloader.classList.remove('loader')
     }
   }
+  function fetchMovieDetails(){
+ 
 
+    loadingStatus = true;
+    checkLoaderStatus()
+    let movieName = document.getElementById('movieName');
+    let apiQuery = API_Movie + movieName.value
+    console.log(apiQuery)
+  
+    fetch(apiQuery).then((response)=>{
+      return response.json()
+    }).then((data)=>{
+      console.log(data)
+      loadingStatus = false;
+      checkLoaderStatus()
+      let img = document.getElementById('img')
+        img.src = data.Poster;
+        let title = document.getElementById('title')
+        title.innerText = data.Title
+        let desc = document.getElementById('desc')
+        desc.innerText = data.Plot
+        let actors = document.getElementById('actors')
+        actors.innerText = 'Actors: ' + data.Actors
+        let awards = document.getElementById('awards')
+        awards.innerText = 'Awards: ' + data.Awards
+        let boxOffice = document.getElementById('boxOffice')
+        boxOffice.innerText = 'Box Office: ' + data.BoxOffice
+        let director = document.getElementById('director')
+        director.innerText = 'Director: ' + data.Director
+        let country = document.getElementById('country')
+        country.innerText = 'Country: ' + data.Country
+        let genre = document.getElementById('genre')
+        genre.innerText = 'Genre: ' + data.Genre
+        let language = document.getElementById('language')
+        language.innerText = 'Language: ' + data.Language
+        
+    
+    })
+  }
 function gen(){
     loadingstatus = true;
     loader()
+   
     let getmoviename = document.getElementById('getmovienames');
     let apiwithmovie = api + getmoviename.value;
     console.log(apiwithmovie);
-    fetch(apiwithmovie).then((response) => {
+    fetch(apiwithmovie)
+        .then((response) => {
             return response.json();
-        }).then((data) => {
-             console.log(data);
+        })
+        .then((data) => {
             loadingstatus = false;
             loader()
-             let h1 = document.getElementById('h1')
-             h1.innerText = data.Title
-    let p = document.getElementById('p')
-    p.innerText = data.Plot
-    let img = document.getElementById('img')
-            img.src = data.Poster  
-        })
+             let h1 = document.getElementById('h1');
+    let p = document.getElementById('p');
+    let img = document.getElementById('img');
+            h1.innerText = data.Title;
+            p.innerText = data.Plot;
+            img.src = data.Poster;
+            console.log(data);
+        });
 }
 // let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t=dangal'
 
