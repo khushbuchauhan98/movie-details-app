50cc3dfddaed2a731272b7939f3dce922447fa77
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 11:04:30 +0530

    f3

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t='
-let loadingstatus = false
-loader = () => {
-    let getloader = document.getElementById('loader')
-    let getintro = document.getElementById('intro')
+
+let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
+let loadingstatus = false;
+function loader(){
+    let getintro = document.getElementById('intro');
     getintro.style.display = 'none';
+    let getloader = document.getElementById('loader');
+  
     if (loadingstatus == true) {
         getloader.classList.add('loader')
     }
     else {
         getloader.classList.remove('loader')
     }
+  }
 
-}
-gen = () => {
-    loadingstatus = true
+function gen(){
+    loadingstatus = true;
     loader()
-   
-    let getmoviename = document.getElementById('getmoviename');
+    let getmoviename = document.getElementById('getmovienames');
     let apiwithmovie = api + getmoviename.value;
-    // console.log(apiwithmovie);
-    fetch(apiwithmovie)
-        .then((response) => {
+    console.log(apiwithmovie);
+    fetch(apiwithmovie).then((response) => {
             return response.json();
-        })
-        .then((data) => {
+        }).then((data) => {
+             console.log(data);
             loadingstatus = false;
             loader()
-             let h1 = document.getElementById('h1');
-    let p = document.getElementById('p');
-    let img = document.getElementById('img');
-            h1.innerText = data.Title;
-            p.innerText = data.Plot;
-            img.src = data.Poster;
-            // console.log(data);
-        });
+             let h1 = document.getElementById('h1')
+             h1.innerText = data.Title
+    let p = document.getElementById('p')
+    p.innerText = data.Plot
+    let img = document.getElementById('img')
+            img.src = data.Poster  
+        })
 }
 // let api = 'http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t=dangal'
 
